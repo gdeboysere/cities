01a1c12efd66dcfa6f818b6d5d34f378253290f1
Grégoire De Boysère <gdeboysere@gmail.com>
2017-03-01 13:17:34 +0100

    adding a few html files (routing)

---
+++ client/routes.js
@@ -0,0 +1,40 @@
+Router.configure({
+    layoutTemplate: "main",
+    notFoundTemplate: "notFound"
+});
+
+Router.route("/", {
+    template: "home"
+});
+
+Router.route('/city/:id', {
+    template: "city",
+    data: function () {
+        // Return the document for the selected city (the one whose id is given)
+        // The value of this id is given by  this.params.id
+    }
+});
+
+Router.route('/event/:id', {
+    template: "event",
+    data: function () {
+        // Return the document for the selected city (the one whose id is given)
+        // The value of this id is given by  this.params.id
+    }
+});
+
+Router.route('/charts/', {
+    template: "charts"
+});
+
+Router.route('/about/', {
+    template: "about"
+});
+
+Router.route('/search/:result', {
+    template: "search",
+    data: function () {
+        // Return the document for the selected city (the one whose id is given)
+        // The value of this id is given by  this.params.id
+    }
+});
