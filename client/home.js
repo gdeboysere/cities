422c3f7a252b58ff001513982668cede5e6d3a11
Julien Aballea <julien.aballea@102f08.iut-lens.univ-artois.fr>
2017-03-01 20:33:47 +0100

    citypageroute

--- client/home.js
+++ client/home.js
@@ -1,6 +1,5 @@
 Template.home.helpers({
     "listdisplay" : function() {
-        console.log( Cities.find({})); 
        return Cities.find({}); 
     }
     
--- client/routes.js
+++ client/routes.js
@@ -7,13 +7,14 @@ Router.route("/", {
     template: "home"
 });
 
-// Router.route('/city/:id', {
-//     template: "city",
-//     data: function () {
-//         // Return the document for the selected city (the one whose id is given)
-//         // The value of this id is given by  this.params.id
-//     }
-// });
+ Router.route('/city/:id', {
+     template: "city",
+     data: function () {
+         return Cities.findOne(this.params.id);
+         // Return the document for the selected city (the one whose id is given)
+         // The value of this id is given by  this.params.id
+     }
+ });
 
 // Router.route('/event/:id', {
 //     template: "event",
